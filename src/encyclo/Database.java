d52127b6ae7b8e13e250fa7c054203a8dbb8f450
pascalr <pascal.rainville@gmail.com>
2012-02-14 22:51:00 -0500

    ...

--- src/encyclo/Database.java
+++ src/encyclo/Database.java
@@ -3,8 +3,13 @@ package encyclo;
 import java.util.HashMap;
 import java.util.Map;
 
+import encyclo.Models.Entity;
+
 public class Database {
 
 	private static final Map TABLES = new HashMap<String, String>();
+	
+	private Entity entity = Entity.newBuilder().build();
+	
 	
 }
